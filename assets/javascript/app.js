bbca534bbfc0be90aaaa4650a48b747419cac869
glory2hypnotoad <matthew.limbach1@icloud.com>
2019-04-12 12:14:47 -0400

    working database and bot

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,32 @@
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://discordapp.com/api/channels/563834169579405341/messages",
-  "method": "GET",
-  "headers": {
-    "Authorization": "Bot NTY1OTY1NDQ2MzkwNDgwOTA2.XLCe1w.ACa_D7B90zXvjjZ6ynfGt5GChwg",
-    "cache-control": "no-cache",
-    "Postman-Token": "8ad414b0-260c-43a8-add3-42e2f058d4e1"
-  }
-}
+$(document).ready(function (){
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-})
-.then(function(response) {
-  console.log(response.data);
+  var config = {
+    apiKey: "AIzaSyCI3XGNh4Nvo8rXYDWnVc7KYbNdwxTRbYY",
+    authDomain: "phila-pokedex.firebaseapp.com",
+    databaseURL: "https://phila-pokedex.firebaseio.com",
+    projectId: "phila-pokedex",
+    storageBucket: "phila-pokedex.appspot.com",
+    messagingSenderId: "678564572304"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
+    var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://cors-anywhere.herokuapp.com/https://discordapp.com/api/channels/563834169579405341/messages?limit=100",
+    "method": "GET",
+    "headers": {
+      "Authorization": "Bot NTY1OTY1NDQ2MzkwNDgwOTA2.XLCe1w.ACa_D7B90zXvjjZ6ynfGt5GChwg",
+      "cache-control": "no-cache",
+      "Postman-Token": "8ad414b0-260c-43a8-add3-42e2f058d4e1",
+    }
+  }
+
+  $.ajax(settings).then(function (response) {
+    console.log(response);
+  })
 
 });
-
